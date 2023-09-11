bbe26ddf96fa46530f7374331e940a3f1843a76c
Sharon Yakubu <sharonyak15@gmail.com>
2023-09-11 12:06:35 +0100

    fetched local json file

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import Profile from "./components/Profile"
 import Daily from "./components/pages/Daily"
 import Weekly from "./components/pages/Weekly"
 import Monthly from "./components/pages/Monthly"
+import "../public/data.json"
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from "react"
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('../public/data.json')
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error('Error fetching data:', error));
+
+      console.log(data)
+  }, []);
+
   return (
     <Router>
       <Routes>
