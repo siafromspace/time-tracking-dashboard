4df580ddf1e30f5a715751356f90a8f8fbcf152f
Sharon Yakubu <sharonyak15@gmail.com>
2023-09-12 18:46:27 +0100

    media queries done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 import Image from "../assets/images/image-jeremy.png"
 
 export default function Profile() {
+  const active = ({isActive}) => isActive ? "active" : ""
   return (
     <div className='profile'>
       <div className='profile-main'>
         <img src={Image} alt="profile image" />
-        <p>Report for</p>
-        <p className='name'>Jeremy Robson</p>
+        <div>
+          <p>Report for</p>
+          <p className='name'>Jeremy Robson</p>
+        </div>
       </div>
       <nav className='profile-links'>
-        <Link to="/daily">Daily</Link>
-        <Link to="/weekly">Weekly</Link>
-        <Link to="/monthly">Monthly</Link>
+        <NavLink to="/daily" className={active}>Daily</NavLink>
+        <NavLink to="/weekly" className={active}>Weekly</NavLink>
+        <NavLink to="/monthly" className={active}>Monthly</NavLink>
       </nav>
     </div>
   )
